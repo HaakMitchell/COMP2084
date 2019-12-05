1a350c8a57f254319c493f057b9097c3343fbd54
Mitchell Haak <200329261@student.georgianc.on.ca>
2019-12-04 21:34:46 -0500

    Added API

---
+++ COMP2084Store/API/BrandsManagerController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using COMP2084Store.Data;
+using COMP2084Store.Models;
+
+namespace COMP2084Store.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsManagerController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BrandsManagerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BrandsManager
+        [HttpGet]
+        public IEnumerable<Brands> GetBrands()
+        {
+            return _context.Brands;
+        }
+
+        // GET: api/BrandsManager/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBrands([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var brands = await _context.Brands.FindAsync(id);
+
+            if (brands == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(brands);
+        }
+
+        // PUT: api/BrandsManager/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBrands([FromRoute] int id, [FromBody] Brands brands)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != brands.BrandId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(brands).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BrandsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BrandsManager
+        [HttpPost]
+        public async Task<IActionResult> PostBrands([FromBody] Brands brands)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Brands.Add(brands);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBrands", new { id = brands.BrandId }, brands);
+        }
+
+        // DELETE: api/BrandsManager/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBrands([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var brands = await _context.Brands.FindAsync(id);
+            if (brands == null)
+            {
+                return NotFound();
+            }
+
+            _context.Brands.Remove(brands);
+            await _context.SaveChangesAsync();
+
+            return Ok(brands);
+        }
+
+        private bool BrandsExists(int id)
+        {
+            return _context.Brands.Any(e => e.BrandId == id);
+        }
+    }
+}
